02969d530d526bbf586984f41cb7f57081174956
fady <fadyjrjs891@gmail.com>
2026-04-10 01:16:35 +0200

    link port in .env

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
